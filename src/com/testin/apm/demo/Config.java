7884754f19ddb546b1576e7ec9d615e85ca44fc2
Jia Zhikai <jiazhikai@testin.cn>
2015-07-03 15:57:10 +0800

    Add test url for http 302

--- src/com/testin/apm/demo/Config.java
+++ src/com/testin/apm/demo/Config.java
@@ -21,6 +21,7 @@ public class Config {
         add("http://www.baidu.com/");
         add("https://download.oneapm.com/android_agent/eclipse/");
         add("http://crash.testin.cn/cpi/crash");
+        add("http://121.40.57.182:8080/apiserver/new/test/error");
     } };
 
     private static final List<String> DECODE_METHODS = new ArrayList<String>() { {
